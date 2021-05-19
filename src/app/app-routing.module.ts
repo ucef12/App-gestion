320a97c2fac7855885ad97265749383e22433332
ucef12 <youssef.amrani417@gmail.com>
2021-05-19 17:48:58 +0100

    create pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'products',component:ProductsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
